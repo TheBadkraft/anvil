e020119c2488707603d46c275f568c072dc9d5b8
David Boarman <thebadkraft@gmail.com>
2025-12-03 18:47:08 -0600

    setting up the project

---
+++ core/cli/main.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Quantum Override. All rights reserved.
+ *
+ * This software is proprietary and confidential. Unauthorized copying,
+ * distribution, modification, or use of this software, via any medium,
+ * is strictly prohibited without express written permission from the
+ * copyright holder.
+ */
+
+ // core/cli/main.c
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    printf("Welcome to Anvil\n");
+    //  TODO: Update version from build system
+    printf("Version 1.0 · December 2025 · Quantum Override\n");
+    
+    return 0;
+}
