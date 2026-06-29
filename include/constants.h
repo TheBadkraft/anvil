0a5f2737e990ba9d7ef4d4827ca504c28243e08d
BadKraft <thebadkraft@gmail.com>
2026-06-29 13:10:57 +0000

    update version patch [v0.5.4]

--- include/constants.h
+++ include/constants.h
@@ -30,9 +30,9 @@
 /* ------------------------------------------------------------------ */
 #define ANVL_VERSION_MAJOR 0
 #define ANVL_VERSION_MINOR 5
-#define ANVL_VERSION_PATCH 3
+#define ANVL_VERSION_PATCH 4
 #define ANVL_VERSION_TAG   "alpha"
-#define ANVL_VERSION_STR   "0.5.3-alpha"
+#define ANVL_VERSION_STR   "0.5.4-alpha"
 
 /* ------------------------------------------------------------------ */
 /* Dialect: AML vs ASL vs AMP vs Aurora                               */
